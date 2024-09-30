ce5145b005621bcc780deac392c9d08b4a950627
KseniaLF <llf9391@gmail.com>
2024-09-30 22:12:17 +0300

    feat: add animated loader on transcribing page

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import HomePage from "./components/HomePage";
 import FileDisplay from "./components/FileDisplay";
+import Information from "./components/Information";
+import Transcribing from "./components/Transcribing";
 
 function App() {
   const [file, setFile] = useState(null);
   const [audioStream, setAudioStream] = useState(null);
+  const [output, setOutput] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   const isAudioAvailible = file || audioStream;
 
   function handleAudioReset() {
@@ -21,7 +26,11 @@ function App() {
     <div className="fex flex-col p-4 max-w-[1000px] mx-auto w-full">
       <section className="min-h-screen flex flex-col">
         <Header />
-        {isAudioAvailible ? (
+        {output ? (
+          <Information />
+        ) : loading ? (
+          <Transcribing />
+        ) : isAudioAvailible ? (
           <FileDisplay
             file={file}
             audioStream={audioStream}
--- src/components/Transcribing.jsx
+++ src/components/Transcribing.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
-export default function Transcribing() {
+export default function Transcribing(props) {
+  const { downloading } = props;
+
   return (
-    <div className="flex items-center flex-col justify-center gap-10 ms:gap-14 py-24"></div>
+    <div className="flex items-center flex-col flex-1 justify-center gap-10 ms:gap-14 pb-24 p-4 text-center">
+      <div className="flex flex-col gap-2 sm:gap-4">
+        <h2 className="text-blue-400 bold font-semibold text-4xl sm:text-5xl md:text-6xl">
+          Your transcribing
+        </h2>
+
+        <p>{!downloading ? "warm up cylinders" : "core cylinders engaged"}</p>
+      </div>
+
+      <div className="flex flex-col gap-2 sm:gap-4 max-w-[400px] mx-auto w-full">
+        {[1, 2, 3].map((val) => {
+          return (
+            <div
+              key={val}
+              className={
+                "rounded-full h-2 sm:h-3 bg-slate-400 loading " +
+                `loading${val}`
+              }
+            ></div>
+          );
+        })}
+      </div>
+    </div>
   );
 }
